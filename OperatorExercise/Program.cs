fae2018ed2282607c842ceb8108d3b3ecbbad87c
Rubi Meneses <rubi.ventura@icloud.com>
2022-08-09 15:05:59 -0500

    exercise two

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -19,6 +19,25 @@ namespace OperatorExercise
 
             //write out results as 17/4 is 4 remainder 1
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            //EXERCISE TWO-
+            //allowing user input
+            Console.WriteLine("Please enter the radius of your circle");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            //calling the method
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+
+        //EXERCISE TWO: create a method that will calculate the area of a circle based on its radius
+        //declaring the AreaofCircle method
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
         }
     }
 }
