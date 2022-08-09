fddb00facee075792423c97031a059fde0fc662f
Rubi Meneses <rubi.ventura@icloud.com>
2022-08-09 14:28:01 -0500

    exercise one

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create two integer variables
+            var a = 17;
+            var b = 4;
+
+            //create two more integer variables
+            var quotient = a / b;
+            var remainder = a % b;
+
+            //use an if statement
+            if (a == 17 && b == 4) ;
+
+            //write out results as 17/4 is 4 remainder 1
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+        }
+    }
+}
+
